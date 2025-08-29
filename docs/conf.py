1a350ee3d0c4cfe9a0ff9520d271137f1c1faa9f
Ginger Hawthorne <gulopine@gmail.com>
2025-08-29 13:00:37 -0400

    Simplify Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,5 @@ project = 'Steel'
 copyright = '2025, Ginger Hawthorne'
 author = 'Ginger Hawthorne'
 
-templates_path = ['_templates']
-html_static_path = ['_static']
-
 extensions = ['sphinx_rtd_theme']
 html_theme = 'sphinx_rtd_theme'

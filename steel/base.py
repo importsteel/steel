f400f9ed9d0f420fda934cb1524b6cdf5fa09b63
Ginger Hawthorne <gulopine@gmail.com>
2025-09-04 16:16:17 -0400

    Add read() and write() and methods to structures

--- steel/base.py
+++ steel/base.py
@@ -1,6 +1,9 @@
-from typing import Any, ClassVar
+from io import BufferedIOBase
+from typing import Any, ClassVar, TypeVar
 
 from .types import AnyField, FieldType
+
+T = TypeVar("T", bound="Structure")
 
 
 class FieldDict(dict[str, AnyField]): ...
@@ -42,3 +45,24 @@ class Structure:
             overrides = override_keys & set(field.all_options) - set(field.specified_options)
             for key in overrides:
                 setattr(field, key, options[key])
+
+    def __init__(self, **kwargs: Any):
+        # FIXME: This needs to be *a lot* smarter, but it proves the basic idea for now
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def read(cls: type[T], buffer: BufferedIOBase) -> T:
+        # FIXME: This needs to be *a lot* smarter, but it proves the basic idea for now
+        data = {}
+        for field in cls._config.fields.values():
+            data[field.name], size = field.read(buffer)
+        return cls(**data)
+
+    def write(self, buffer: BufferedIOBase) -> int:
+        # FIXME: This needs to be *a lot* smarter, but it proves the basic idea for now
+        size = 0
+        for field in self.__class__._config.fields.values():
+            value = getattr(self, field.name)
+            size += field.write(value, buffer)
+        return size
--- steel/types.py
+++ steel/types.py
@@ -1,3 +1,4 @@
+from io import BufferedIOBase
 from typing import Any, ClassVar
 
 
@@ -5,6 +6,12 @@ class FieldType[T, D]:
     name: str
     all_options: ClassVar[dict[str, Any]]
     specified_options: dict[str, Any]
+
+    def read(self, buffer: BufferedIOBase) -> tuple[T, int]:
+        raise NotImplementedError()
+
+    def write(self, value: T, buffer: BufferedIOBase) -> int:
+        raise NotImplementedError()
 
 
 type AnyField = FieldType[Any, Any]

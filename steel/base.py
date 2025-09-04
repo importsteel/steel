141e5e315158832ba3e81026c3cbe6475c4a4aa7
Ginger Hawthorne <gulopine@gmail.com>
2025-09-04 12:53:38 -0400

    Structures can track fields and configurations

--- steel/base.py
+++ steel/base.py
@@ -1,2 +1,44 @@
+from typing import Any, ClassVar
+
+from .types import AnyField, FieldType
+
+
+class FieldDict(dict[str, AnyField]): ...
+
+
+class Configuration:
+    fields: FieldDict
+    options: dict[str, Any]
+
+    def __init__(self, options: dict[str, Any]) -> None:
+        self.fields = FieldDict()
+        self.options = options
+
+    def add_field(self, field: AnyField) -> None:
+        self.fields[field.name] = field
+
+    def __getitem__(self, name: str) -> AnyField:
+        return self.fields[name]
+
+
 class Structure:
-    pass
+    _config: ClassVar[Configuration]
+
+    def __init_subclass__(cls, **options: Any) -> None:
+        super().__init_subclass__()
+        override_keys = set(options.keys())
+        cls._config = Configuration(options=options)
+        for attr in cls.__dict__.values():
+            if not isinstance(attr, FieldType):
+                continue
+            # Just to make it clear from here on out
+            field = attr
+            cls._config.add_field(field)
+
+            if not override_keys:
+                # No extra options passed in, so there's nothing more to do with this field
+                continue
+
+            overrides = override_keys & set(field.all_options) - set(field.specified_options)
+            for key in overrides:
+                setattr(field, key, options[key])
--- steel/types.py
+++ steel/types.py
@@ -4,7 +4,6 @@ from typing import Any, ClassVar
 class FieldType[T, D]:
     name: str
     all_options: ClassVar[dict[str, Any]]
-    specified_arguments: tuple[Any]
     specified_options: dict[str, Any]
 
 
